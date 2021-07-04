36c371cfa65f052638934b1218a9ac3f6382f63e
jungheejung <heejung.jung@colorado.edu>
2021-07-04 17:56:39 -0400

    fmriprep script

--- fmriprep/fmriprep_slurm/simple_fmriprep.sh
+++ fmriprep/fmriprep_slurm/simple_fmriprep.sh
@@ -8,7 +8,7 @@
 #SBATCH -e ./log/preproc_%A_%a.e
 #SBATCH --account=DBIC
 #SBATCH --partition=standard
-#SBATCH --array=6
+#SBATCH --array=11,12,15,16
 ## --array=1-17%5
 
 
@@ -16,16 +16,17 @@
 #conda activate spacetop_env
 
 # parameters _________________________________________________________
-CONTAINER_IMAGE="/dartfs-hpc/rc/lab/C/CANlab/modules/fmriprep-20.2.1-LTS.sif"
-MAINDIR="/dartfs-hpc/rc/lab/C/CANlab/labdata/data/spacetop"
-BIDS_DIRECTORY="${MAINDIR}/dartmouth"
-SCRATCH_DIR="/scratch/f0042x1/spacetop/preproc"
-SCRATCH_WORK="${SCRATCH_DIR}/work"
-OUTPUT_DIR="${MAINDIR}/derivatives/dartmouth/fmriprep"
-OUTPUT_WORK="${OUTPUT_DIR}/work"
+CONTAINER_IMAGE=/dartfs-hpc/rc/lab/C/CANlab/modules/fmriprep-20.2.1-LTS.sif
+MAINDIR=/dartfs-hpc/rc/lab/C/CANlab/labdata/data/spacetop
+BIDS_DIRECTORY=${MAINDIR}/dartmouth
+SCRATCH_DIR=/scratch/f0042x1/spacetop/preproc
+SCRATCH_WORK=${SCRATCH_DIR}/work
+OUTPUT_DIR=${MAINDIR}/derivatives/dartmouth/fmriprep
+OUTPUT_WORK=${OUTPUT_DIR}/work
 
-subjects=("0001" "0002" "0003" "0004" "0005" "0006" "0007" "0008" "0009" "0010" \
+subjects=("0001" "0002" "0003" "0004" "0005"  "0007" "0008" "0009" "0010" \
 "0011" "0013" "0014" "0015" "0016" "0017" "0020")
+# "0006"
 PARTICIPANT_LABEL=${subjects[$((SLURM_ARRAY_TASK_ID - 1 ))]}
 echo "array id: " ${SLURM_ARRAY_TASK_ID}, "subject id: " ${PARTICIPANT_LABEL}
 
@@ -38,15 +39,14 @@ datalad clean
 
 module purge
 module load freesurfer/6.0.0
-
-#singularity run -B /dartfs-hpc/rc/lab/C/CANlab/labdata/data/spacetop/dartmouth:/dartfs-hpc/rc/lab/C/CANlab/labdata/data/spacetop/dartmouth  /dartfs-hpc/rc/lab/C/CANlab/modules/mriqc-0.14.2.sif /dartfs-hpc/rc/lab/C/CANlab/labdata/data/spacetop/dartmouth /dartfs-hpc/rc/lab/C/CANlab/labdata/data/spacetop/dartmouth/derivatives participant --participant_label 0005
 unset $PYTHONPATH;
 
-singularity run -B ${BIDS_DIRECTORY}:${BIDS_DIRECTORY} \
+# singularity run -B ${BIDS_DIRECTORY}:${BIDS_DIRECTORY} \
+singularity exec --bind ${BIDS_DIRECTORY}:${BIDS_DIRECTORY} \
 -B ${OUTPUT_DIR}:${OUTPUT_DIR} \
 -B ${OUTPUT_WORK}:${OUTPUT_WORK} \
 -B /optnfs/freesurfer:/optnfs/freesurfer \
-${CONTAINER_IMAGE} \
+${CONTAINER_IMAGE}  fmriprep \
 ${BIDS_DIRECTORY} ${OUTPUT_DIR} participant --participant_label ${PARTICIPANT_LABEL} -w ${OUTPUT_WORK} \
 --task-id social \
 --write-graph \
@@ -54,15 +54,16 @@ ${BIDS_DIRECTORY} ${OUTPUT_DIR} participant --participant_label ${PARTICIPANT_LA
 --notrack \
 --mem_mb 48000 \
 --bold2t1w-dof 9 --dummy-scans 6 \
---use-aroma  --fd-spike-threshold 0.9  --cifti-output 91k \
+--fd-spike-threshold 0.9  \
+--fs-no-reconall \
 --fs-license-file /optnfs/freesurfer/6.0.0/license.txt \
 # ${BIDS_DIRECTORY} ${OUTPUT_DIR} participant --participant_label ${PARTICIPANT_LABEL} -w ${OUTPUT_WORK}
 
 
 echo "COMPLETING fmriprep ... COPYING over"
 
-# cp ${SCRATCH_DIR} ${OUTPUT_DIR}
-# cp ${SCRATCH_WORK} ${OUTPUT_WORK}
+#cp ${SCRATCH_DIR} ${OUTPUT_DIR}
+#cp ${SCRATCH_WORK} ${OUTPUT_WORK}
 
 echo "process complete"
 
