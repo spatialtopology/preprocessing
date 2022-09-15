14c9957e6dac0d794a99b797123b93c21b445342
jungheejung <>
2022-09-15 14:47:19 -0400

    ENH: load full data - separate into runs and save as BIDS format

--- biopac/s01_bidsify/c02_save_separate_run.py
+++ biopac/s01_bidsify/c02_save_separate_run.py
@@ -25,6 +25,7 @@ import itertools
 import os, shutil, glob
 from pathlib import Path
 import json
+from itertools import compress
 
 # %% temporary
 main_dir = '/Volumes/spacetop'
@@ -32,30 +33,33 @@ print(main_dir)
 save_dir = os.path.join(main_dir, 'biopac', 'dartmouth', 'b04_finalbids')
 print(save_dir)
 
+
+
 # %%
 # 1. glob acquisition files __________________________
 # filename ='/Users/h/Dropbox/projects_dropbox/spacetop_biopac/data/sub-0026/SOCIAL_spacetop_sub-0026_ses-01_task-social_ANISO.acq'
-acq_list = glob.glob(os.path.join(main_dir, 'biopac', 'dartmouth', 'b02_sorted', 'sub-' + ('[0-9]' * 4), '*', '*task-social*_ANISO.acq'), recursive = True)
+acq_list = glob.glob(os.path.join(main_dir, 'biopac', 'dartmouth', 'b02_sorted', 'sub-*', '**', '*task-social*.acq'), recursive = True)
 flaglist = []
 
-for acq in acq_list:
+# %%
+for acq in sorted(acq_list):
     # 2. extract information from filenames __________________________
     filename  = os.path.basename(acq)
     sub = [match for match in filename.split('_') if "sub" in match][0]
     ses = [match for match in filename.split('_') if "ses" in match][0] # 'ses-03'
     task = [match for match in filename.split('_') if "task" in match][0]
 
-    entities = dict(
-    match.split('-')
-    for match in filename.split('_')
-    if '-' in match
-    )
+    # entities = dict(
+    # match.split('-')
+    # for match in filename.split('_')
+    # if '-' in match
+    # )
 
     try: 
         spacetop_data, spacetop_samplingrate = nk.read_acqknowledge(acq)
 
 
-        # %% 3. 3. from the "trigger" channel
+        #  3. 3. from the "trigger" channel
         # 1) binarize signals
         mid_val = (np.max(spacetop_data['trigger']) - np.min(spacetop_data['trigger']))/2
         spacetop_data.loc[spacetop_data['trigger'] > mid_val, 'fmri_trigger'] = 5
@@ -65,12 +69,24 @@ for acq in acq_list:
 
         start_df = spacetop_data[spacetop_data['fmri_trigger'] > spacetop_data[ 'fmri_trigger'].shift(1)].index
         stop_df = spacetop_data[spacetop_data['fmri_trigger'] < spacetop_data[ 'fmri_trigger'].shift(1)].index
-        for r in range(len(start_df)):
+
+        # 3) if start_df and stop_df difference is greater than 300 seconds, include. 
+        # 4) if not, exclude from dataset
+        # 5) pop run number
+        run_list = list(range(len(start_df)))
+        run_bool = ((stop_df-start_df)/spacetop_samplingrate)>300
+        clean_runlist = list(compress(run_list, run_bool))
+        shorter_than_300 = list(compress(run_list, ~run_bool))
+        if len(shorter_than_300) > 0:
+            flaglist.append(f"runs shorter than 300 sec: {sub} {ses} {shorter_than_300} - run number in python order")
+        
+        for r in clean_runlist:
             run_df = spacetop_data.iloc[start_df[ r]:stop_df[ r]]
-            run_basename = f'{sub}_{ses}_{task}_run-{r:02d}_recording-ppg-eda_physio.acq'
+            run_basename = f'{sub}_{ses}_{task}_run-{r+1:02d}_recording-ppg-eda_physio.acq'
             run_dir = os.path.join(save_dir, task, sub, ses )
             Path(run_dir).mkdir( parents=True, exist_ok=True )
             run_df.to_csv(os.path.join(run_dir, run_basename), index = False)
     except:
         flaglist.append(acq_list)
 
+# %%
