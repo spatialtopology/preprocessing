8a4bd75a769a19e365aa6576dcf943e8aeb61170
jungheejung <>
2022-10-11 12:13:21 -0400

    STY: update comments

--- biopac/c02_save_separate_run.py
+++ biopac/c02_save_separate_run.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
-""" Separates acq into runs and saves as bids format. 
+""" 
+save_seperate_run.py
+Separates acq into runs and saves as bids format. 
 This script reads the acquisition file collected straight from our biopac PC
 
 Parameters
@@ -12,27 +14,6 @@ cutoff_threshold: int
     threshold for determining "kosher" runs versus not. 
     for instance, task-social is 398 seconds long. I use the threshold of 300 as a threshold. 
     Anything shorter than that is discarded and not converted into a run
-
-Steps (TODO coding)
-------------------
-1) [x] glob acquisitions files
-2) [x] extract information from filesnames
-3) [x] binarize signals based on MR Trigger channel (received RF pulse)
-4) [x] convert dataframe to seconds, instead of 2000 sampling rate.
-5) [x] identify transitions
-6) [x] check if transition is longer than expected TR (threshold 300 s)
-6-1) if longer than threshold, include and save as separate run
-6-2) if less than expected, flag and keep a note in the flatlist. Pop that index using boolean mask. 
-7) [x] save using bids naming convention
-
-Questions: 
-------------------
-1) What if the data is shorter than expected run 
-A: depending on the thresdhold you provide, the code will identify a block of timepoints as a run or not.
-
-2) what if data is longer than expected (e.g. forgot to start and stop run)?
-A: No worries, we're using the channel with the MRtriggers "fMRI Trigger - CBLCFMA - Current Feedba"
-The data can't be longer than the MRI protocol, if the criteria is based on the MRtriggers ;)
 """
 
 # %% libraries ________________________
@@ -65,8 +46,7 @@ from utils import preprocess, initialize
 
 __author__ = "Heejung Jung"
 __copyright__ = "Spatial Topology Project"
-# people who reported bug fixes, made suggestions, etc. but did not actually write the code.
-__credits__ = ["Heejung"]
+__credits__ = ["Heejung"] # people who reported bug fixes, made suggestions, etc. but did not actually write the code.
 __license__ = "GPL"
 __version__ = "0.0.1"
 __maintainer__ = "Heejung Jung"
@@ -113,7 +93,6 @@ print(biopac_dir)
 print(save_dir)
 
 # set up logger _______________________________________________________________________________________
-
 runmeta = pd.read_csv(
     join(spacetop_dir, "data/spacetop_task-social_run-metadata.csv"))
 #TODO: come up with scheme to update logger files
@@ -124,8 +103,8 @@ f = open(logger_fname, "w")
 logger = utils.initialize._logger(logger_fname)
 
 
-# %% 1. glob acquisition files ____________________________________________________________________________
-# filename ='/Users/h/Dropbox/projects_dropbox/spacetop_biopac/data/sub-0026/SOCIAL_spacetop_sub-0026_ses-01_task-social_ANISO.acq'
+# %% NOTE: 1. glob acquisition files ____________________________________________________________________________
+# filename ='../spacetop_biopac/data/sub-0026/SOCIAL_spacetop_sub-0026_ses-01_task-social_ANISO.acq'
 remove_int = [1, 2, 3, 4, 5, 6]
 sub_list = utils.initialize._sublist(source_dir, remove_int, slurm_ind, stride=10, sub_zeropad=4)
 
@@ -137,16 +116,17 @@ for sub in sub_list:
     acq_list.append(acq)
 flat_acq_list = [item for sublist in acq_list  for item in sublist]
 print(flat_acq_list)
+
 # %%
 for acq in sorted(flat_acq_list):
-# NOTE: extract information from filenames _______________________________________________________________
+# NOTE: 2. extract information from filenames _______________________________________________________________
     filename = os.path.basename(acq)
     bids_dict = {}
-    bids_dict['sub']=  sub = utils.initialize._extract_bids(filename, 'sub')
-    bids_dict['ses']= ses = utils.initialize._extract_bids(filename, 'ses')
+    bids_dict['sub'] = sub  = utils.initialize._extract_bids(filename, 'sub')
+    bids_dict['ses'] = ses  = utils.initialize._extract_bids(filename, 'ses')
     bids_dict['task']= task = utils.initialize._extract_bids(filename, 'task')
 
-# NOTE: open physio dataframe (check if exists) __________________________________________________________
+# NOTE: 3. open physio dataframe (check if exists) __________________________________________________________
     if os.path.exists(acq):
         main_df, samplingrate = nk.read_acqknowledge(acq)
         logger.info(f"\n\n__________________{sub} {ses} __________________")
@@ -155,7 +135,7 @@ for acq in sorted(flat_acq_list):
         logger.error(f"\tno biopac file exists")
         continue
         
-# NOTE create an mr_aniso channel for TTL channel _________________________________________________________________________
+# NOTE: 4. create an mr_aniso channel for MRI RF pulse channel ________________________________________________
     try:
         main_df['mr_aniso'] = main_df[dict_column['fMRI_ttl']].rolling(
         window=3).mean()
@@ -179,7 +159,8 @@ for acq in sorted(flat_acq_list):
     logger.info(f"number of spikes within experiment: {len(dict_spike['start'])}")
     main_df['bin_spike'] = 0
     main_df.loc[dict_spike['start'], 'bin_spike'] = 5
-    # 2)
+    
+# NOTE: 5. create an mr_aniso channel for MRI RF pulse channel ________________________________________________
     try:
         main_df['mr_aniso_boxcar'] = main_df[dict_column['fMRI_ttl']].rolling(
             window=int(samplingrate-100)).mean()
@@ -200,7 +181,7 @@ for acq in sorted(flat_acq_list):
     logger.info(f"* stop_df: {dict_runs['stop']}")
     logger.info(f"* total of {len(dict_runs['start'])} runs")
 
-# NOTE: _____________ adjust one TR (remove it!)__________________________________________________________
+# NOTE: 6. adjust one TR (remove it!)_________________________________________________________________________
     sdf = main_df.copy()
     sdf.loc[dict_runs['start'], 'bin_spike'] = 0
     sdf['adjusted_boxcar'] = sdf['bin_spike'].rolling(window=int(samplingrate-100)).mean()
@@ -216,7 +197,7 @@ for acq in sorted(flat_acq_list):
     print(f"* adjusted start_df: {dict_runs_adjust['start']}")
     print(f"* adjusted stop_df: {dict_runs_adjust['stop']}")
 
-    # 2) identify transitions
+# NOTE: 7. identify run transitions ___________________________________________________________________________
     run_list = list(range(len(dict_runs_adjust['start'])))
     try:
         run_bool = ((np.array(dict_runs_adjust['stop'])-np.array(dict_runs_adjust['start']))/samplingrate) > run_cutoff
@@ -227,7 +208,7 @@ for acq in sorted(flat_acq_list):
     clean_runlist = list(compress(run_list, run_bool))
     shorter_than_threshold_length = list(compress(run_list, ~run_bool))
 
-# NOTE: _____________ save identified runs after cross referencing metadata __________________________________________________________
+# NOTE: 8. save identified runs after cross referencing metadata __________________________________________________________
     if len(shorter_than_threshold_length) > 0:
         logger.info(
             f"runs shorter than {run_cutoff} sec: {sub} {ses} {shorter_than_threshold_length} - run number in python order")
