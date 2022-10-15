1cd8e06ab011e9c3ed99568901ec9a803541d169
jungheejung <heejung.jung@colorado.edu>
2022-10-14 22:25:58 -0400

    DEV: submission script

---
+++ spacetop-prep/physio/c02_SUBMIT.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -l
+#SBATCH --job-name=biopac
+#SBATCH --nodes=1
+#SBATCH --task=4
+#SBATCH --mem-per-cpu=8gb
+#SBATCH --time=00:30:00
+#SBATCH -o ./log/biopac_%A_%a.o
+#SBATCH -e ./log/biopac_%A_%a.e
+#SBATCH --account=DBIC
+#SBATCH --partition=standard
+#SBATCH --array=3-14%5
+
+conda activate biopac
+
+CLUSTER="discovery" # local
+SLURM_IND=${SLURM_ARRAY_TASK_ID}
+TASK="task-social"
+CUTOFF=300
+python ${PWD}/c02_save_separate_run.py ${CLUSTER} ${SLURM_IND} ${TASK} ${CUTOFF}
