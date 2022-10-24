721bc32e437cd8de2d9bdece92122c49c81bf2c2
jungheejung <heejung.jung@colorado.edu>
2022-10-24 17:00:58 -0400

    STY: format lines

--- spacetop-prep/physio/c02_SUBMIT.sh
+++ spacetop-prep/physio/c02_SUBMIT.sh
@@ -8,7 +8,7 @@
 #SBATCH -e ./log/physio02_%A_%a.e
 #SBATCH --account=DBIC
 #SBATCH --partition=standard
-#SBATCH --array=1-14%5
+#SBATCH --array=2-14%5
 
 conda activate biopac
 
@@ -16,4 +16,8 @@ CLUSTER="discovery" # local
 SLURM_ID=${SLURM_ARRAY_TASK_ID}
 TASK="task-social"
 CUTOFF=300
-python ${PWD}/c02_save_separate_run.py --operating ${CLUSTER} --slurm_id ${SLURM_ID} --task ${TASK} --run-cutoff ${CUTOFF}
+python ${PWD}/c02_save_separate_run.py \
+--operating ${CLUSTER} \
+--slurm_id ${SLURM_ID} \
+--task ${TASK} \
+--run-cutoff ${CUTOFF}
