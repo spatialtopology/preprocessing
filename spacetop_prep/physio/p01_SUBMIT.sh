758995b01c3a221f3080ff0b995d07d4ee3f416a
jungheejung <>
2022-11-16 12:52:38 -0500

    DEV: update parser.args arguments

--- spacetop_prep/physio/p01_SUBMIT.sh
+++ spacetop_prep/physio/p01_SUBMIT.sh
@@ -12,20 +12,42 @@
 
 conda activate biopac
 
-CLUSTER="discovery" # local
+# CLUSTER="discovery" # local
+PROJECT_DIR="/dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop_projects_social"
+PHYSIO_DIR="${PROJECT_DIR}/data/physio/physio03_bids"
+BEH_DIR="${PROJECT_DIR}/data/beh/beh02_preproc"
+OUTPUT_LOGDIR="${PROJECT_DIR}/scripts/logcenter"
+OUTPUT_SAVEDIR="${PROJECT_DIR}/analysis/physio"
+METADATA="${PROJECT_DIR}/data/spacetop_task-social_run-metadata.csv"
+CHANNELJSON=""
 SLURM_ID=${SLURM_ARRAY_TASK_ID}
 STRIDE=10
 ZEROPAD=4
-TASK="task-social"
-CUTOFF=300
+TASK="task-cue"
 SAMPLINGRATE=2000
 
 # TODO: these metadata -- cutoff, sampling rate -- could be pulled in from json sidecars
 python ${PWD}/p01_group_level_analysis.py \
---operating ${CLUSTER} \ 
+# --operating ${CLUSTER} \ 
+--input-physiodir ${PHYSIO_DIR} \
+--input-behdir ${BEH_DIR} \
+--output-logdir ${OUTPUT_LOGDIR} \
+--output-savedir ${OUTPUT_SAVEDIR} \
+--metadata ${METADATA} \
+--dictchannel ${}
 --slurm_id ${SLURM_ID} \
 --stride ${STRIDE} \
 --zeropad ${ZEROPAD} \
 --task ${TASK} \
---run-cutoff ${CUTOFF} \
 --sr ${SAMPLINGRATE}
+
+# required parameters
+# input_physio_dir
+# input_beh_dir
+# top_dir needed?
+# out_logdir
+# out_savedir
+# run metadata
+# list of columns to extract from behavioral data 
+# change channel names
+
