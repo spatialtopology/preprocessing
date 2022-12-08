68a65cc8a1e012ec1aa75569053a87c530b53a7b
jungheejung <heejung.jung@colorado.edu>
2022-12-08 13:31:49 -0500

    ENH: fix pandas chain indexing warning

--- spacetop_prep/physio/utils/ttl_extraction.py
+++ spacetop_prep/physio/utils/ttl_extraction.py
@@ -154,8 +154,11 @@ def ttl_extraction(physio_df, dict_beforettl, dict_afterttl, dict_stimuli, sampl
     flat_nans = [item for sublist in any_nans for item in sublist]
     for ind in flat_nans:
         ttl_start = np.delete(ttl_start, ind)
-    metadata_df.drop(flat_nans, axis=0, inplace=True)
+    metadata_df2 = metadata_df.copy()
+    metadata_df2.drop(flat_nans, axis=0, inplace=True)
     # metadata_df['trial_num'] = metadata_df.index + 1
-    metadata_df.loc[:, 'trail_num'] = metadata_df.index + 1
+    index_range = metadata_df2.index
+    metadata_df3 = metadata_df2.assign(trial_num=list(np.array(index_range + 1)))
+    # metadata_df.loc[:,'trial_num'] = list(np.array(index_range + 1))
 
-    return metadata_df, ttl_start
+    return metadata_df3, ttl_start
