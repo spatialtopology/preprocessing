6e9cd54bd7c005b87e3954c36e95f4ce5e262357
jungheejung <heejung.jung@colorado.edu>
2023-05-31 20:06:52 -0400

    BUG: fix argparse typo

--- spacetop_prep/qcplot/meanimg/meanimg01.sh
+++ spacetop_prep/qcplot/meanimg/meanimg01.sh
@@ -15,4 +15,4 @@ conda activate spacetop_env
 echo "SLURMSARRAY: " ${SLURM_ARRAY_TASK_ID}
 ID=$((SLURM_ARRAY_TASK_ID-1))
 MAINDIR='/dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop_projects_cue'
-python ${MAINDIR}/scripts/plotmeanimg.py --slurm_id ${ID}
+python ${MAINDIR}/scripts/plotmeanimg.py --slurm-id ${ID}
