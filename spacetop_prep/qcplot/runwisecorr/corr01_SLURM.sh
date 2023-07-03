359fda28c657d112ddb71f6a595d82d72208faa2
jungheejung <heejung.jung@colorado.edu>
2023-07-03 15:44:18 -0400

    BUG: incorrect directory

--- spacetop_prep/qcplot/runwisecorr/corr01_SLURM.sh
+++ spacetop_prep/qcplot/runwisecorr/corr01_SLURM.sh
@@ -8,14 +8,14 @@
 #SBATCH -e ./logcorr/np_%A_%a.e
 #SBATCH --account=DBIC
 #SBATCH --partition=standard
-#SBATCH --array=1-13%10
+#SBATCH --array=1-3%10
 
 conda activate spacetop_env
 echo "SLURMSARRAY: " ${SLURM_ARRAY_TASK_ID}
 ID=$((SLURM_ARRAY_TASK_ID-1))
 # QCDIR='/dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop_projects_cue'
 MAINDIR='/dartfs-hpc/rc/lab/C/CANlab/labdata/data/spacetop_data/scripts/spacetop_prep/qcplot'
-QCDIR='/dartfs-hpc/rc/lab/C/CANlab/labdata/data/spacetop_data/spacetop_data/derivatives/fmriprep_qc'
+QCDIR='/dartfs-hpc/rc/lab/C/CANlab/labdata/data/spacetop_data/derivatives/fmriprep_qc'
 FMRIPREPDIR='/dartfs-hpc/rc/lab/C/CANlab/labdata/data/spacetop_data/derivatives/fmriprep/results/fmriprep'
 SAVEDIR='/dartfs-hpc/rc/lab/C/CANlab/labdata/data/spacetop_data/spacetop_data/derivatives/fmriprep_qc/runwisecorr'
 SCRATCHDIR='/dartfs-hpc/scratch/f0042x1'
