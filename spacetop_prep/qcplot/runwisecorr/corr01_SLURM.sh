dcee8372cdb09bc2b00cdf5be99cb71669ea1758
jungheejung <heejung.jung@colorado.edu>
2023-07-03 15:22:55 -0400

    BUG: qc directory argument

--- spacetop_prep/qcplot/runwisecorr/corr01_SLURM.sh
+++ spacetop_prep/qcplot/runwisecorr/corr01_SLURM.sh
@@ -15,12 +15,14 @@ echo "SLURMSARRAY: " ${SLURM_ARRAY_TASK_ID}
 ID=$((SLURM_ARRAY_TASK_ID-1))
 # QCDIR='/dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop_projects_cue'
 MAINDIR='/dartfs-hpc/rc/lab/C/CANlab/labdata/data/spacetop_data/scripts/spacetop_prep/qcplot'
+QCDIR='/dartfs-hpc/rc/lab/C/CANlab/labdata/data/spacetop_data/spacetop_data/derivatives/fmriprep_qc'
 FMRIPREPDIR='/dartfs-hpc/rc/lab/C/CANlab/labdata/data/spacetop_data/derivatives/fmriprep/results/fmriprep'
 SAVEDIR='/dartfs-hpc/rc/lab/C/CANlab/labdata/data/spacetop_data/spacetop_data/derivatives/fmriprep_qc/runwisecorr'
 SCRATCHDIR='/dartfs-hpc/scratch/f0042x1'
 CANLABDIR='/dartfs-hpc/rc/lab/C/CANlab/modules/CanlabCore'
 python ${MAINDIR}/runwisecorr/runwisecorr.py \
 --slurm-id ${ID} \
+--qcdir ${QCDIR} \
 --fmriprepdir ${FMRIPREPDIR} \
 --savedir ${SAVEDIR} \
 --scratchdir ${SCRATCHDIR} \
