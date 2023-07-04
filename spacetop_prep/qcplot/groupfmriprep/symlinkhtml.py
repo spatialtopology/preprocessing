2a1ef0c47e0de328a866bc3ab224a9ef6d1fc584
jungheejung <heejung.jung@colorado.edu>
2023-07-03 22:57:51 -0400

    TODO: save fmriprepgr output as standalone html

--- spacetop_prep/qcplot/groupfmriprep/symlinkhtml.py
+++ spacetop_prep/qcplot/groupfmriprep/symlinkhtml.py
@@ -35,3 +35,30 @@ save_path=  '/Users/h/Downloads/consolidated_sdc_001.html'
 resolve_symlinks_in_html(url, save_path)
 
 # %%
+/Volumes/derivatives/fmriprep/results/fmriprep/group/consolidated_sdc_000.html
+        output_file = os.path.join(save_dir, f'{sub}_fmriprepSDC_output.html')
+        svg_files = glob.glob(os.path.join(directory, sub, 'figures/*sdc_bold.svg'), recursive=True)
+        # Create the HTML file
+        with open(output_file, 'w') as html_file:
+            # Write the HTML header
+            html_file.write('<html><body>\n')
+
+            # Iterate over the PNG files
+            for svg_file in sorted(svg_files):
+                # Get the filename without the directory path
+                filename = os.path.basename(svg_file)
+                # filename = svg_file
+                
+                with open(svg_file, 'r') as f:
+                    svg_content = f.read()
+                    svg_html = f'<div>{svg_content}</div>\n'
+
+                # Write the header and image tag for each file
+                    html_file.write('<h2>{}</h2>\n'.format(filename))
+                # html_file.write('<object data="{}" type="image/svg+xml"></object>\n'.format(svg_file))
+                    html_file.write('{}'.format(svg_html))
+                
+            # Write the HTML footer
+            html_file.write('</body></html>\n')
+
+    print('HTML file generated successfully.')
