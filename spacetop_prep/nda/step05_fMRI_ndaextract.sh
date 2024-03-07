f62444b17b26af098e78370d1f368b3f875fa4f8
jungheejung <heejung.jung@colorado.edu>
2024-03-07 17:25:04 -0500

    DEV: nda

--- spacetop_prep/nda/step05_fMRI_ndaextract.sh
+++ spacetop_prep/nda/step05_fMRI_ndaextract.sh
@@ -1,5 +1,6 @@
-
-BIDS_DIRECTORY="/dartfs-hpc/rc/lab/C/CANlab/labdata/data/spacetop_data/derivatives/fmriprep/results/fmriprep"
-GUID_MAPPING="/dartfs-hpc/rc/lab/C/CANlab/labdata/data/spacetop_data/scripts/spacetop_prep/nda/ndar_subject.csv"
+find . -type f -name '*dup-01*' -delete
+datalad get .
+BIDS_DIRECTORY="/dartfs-hpc/rc/lab/C/CANlab/labdata/data/spacetop/dartmouth"
+GUID_MAPPING="/dartfs-hpc/rc/lab/C/CANlab/labdata/data/spacetop_data/scripts/spacetop_prep/nda/GUIDMAPPING.txt"
 OUTPUT_DIRECTORY="/dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop_projects_cueNDA"
 bids2nda ${BIDS_DIRECTORY} ${GUID_MAPPING} ${OUTPUT_DIRECTORY}
