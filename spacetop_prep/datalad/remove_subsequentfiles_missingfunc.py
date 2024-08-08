852b0fcd84f53a29e6579d2d7ec5e392e5c41481
jungheejung <heejung.jung@colorado.edu>
2024-08-08 11:29:39 -0400

    BUG: typo

--- spacetop_prep/datalad/remove_subsequentfiles_missingfunc.py
+++ spacetop_prep/datalad/remove_subsequentfiles_missingfunc.py
@@ -1,7 +1,7 @@
 import os
 import glob
 
-datalad_dir = '/Users/h/Documents/projects_local/1076)spacetop'
+datalad_dir = '/Users/h/Documents/projects_local/1076_spacetop'
 # Define the suffixes for the different file types
 file_suffixes = ['_bold.json', '_bold.nii.gz', '_events.tsv', '_sbref.json', '_sbref.nii.gz']
 
