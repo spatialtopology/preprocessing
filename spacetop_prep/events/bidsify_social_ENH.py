89add340b2d29b22b04d2af0dde33b66e3c5c523
jungheejung <heejung.jung@colorado.edu>
2024-08-23 22:46:06 -0400

    BUG: glob filepath instead of string

--- spacetop_prep/events/bidsify_social_ENH.py
+++ spacetop_prep/events/bidsify_social_ENH.py
@@ -281,9 +281,9 @@ if args.bids_string: # and task_name in args.bids_string:
     filtered_cognitive_flist = glob.glob(str(Path(beh_inputdir) / sub / '**' / 'task-social' / '**' / f'*{args.bids_string}*.csv'), recursive=True)
 
     if not filtered_cognitive_flist:
-        temp_fpath = Path(beh_inputdir) / sub / 'task-social' / ses / f'{sub}_{ses}_task-social_{run}*TEMP*.csv'
-        if temp_fpath.is_file():
-            filtered_cognitive_flist = [str(temp_fpath)]
+        temp_fpath = glob.glob(Path(beh_inputdir) / sub / 'task-social' / ses / f'{sub}_{ses}_task-social_{run}*TEMP*.csv')
+        if temp_fpath[0].is_file():
+            filtered_cognitive_flist = [str(temp_fpath[0])]
         else:
             print(f'No behavior data file found for {sub}, {ses}, {run}. Checked both standard and temporary filenames.')
             filtered_cognitive_flist = []
@@ -521,9 +521,9 @@ if args.bids_string: # and task_name in args.bids_string:
     filtered_pain_flist = glob.glob(str(Path(beh_inputdir) / sub / '**' / 'task-social' / '**' / f'*{args.bids_string}*.csv'), recursive=True)
 
     if not filtered_pain_flist:
-        temp_fpath = Path(beh_inputdir) / sub / 'task-social' / ses / f'{sub}_{ses}_task-social_{run}*TEMP*.csv'
-        if temp_fpath.is_file():
-            filtered_pain_flist = [str(temp_fpath)]
+        temp_fpath = glob.glob(Path(beh_inputdir) / sub / 'task-social' / ses / f'{sub}_{ses}_task-social_{run}*TEMP*.csv')
+        if temp_fpath[0].is_file():
+            filtered_pain_flist = [str(temp_fpath[0])]
         else:
             print(f'No behavior data file found for {sub}, {ses}, {run}. Checked both standard and temporary filenames.')
             filtered_pain_flist = []
@@ -781,16 +781,18 @@ task_name = 'vicarious'
 vicarious_logger = setup_logger('vicarious', 'task-cue_vicarious.log')
 
 if args.bids_string:
-    sub = extract_bids(bids_string, 'sub')
-    ses = extract_bids(bids_string, 'ses')
-    run = extract_bids(bids_string, 'run')
+    fname = Path(args.bids_string).name
+    sub = extract_bids(fname, 'sub')
+    ses = extract_bids(fname, 'ses')
+    run = extract_bids(fname, 'run')
     # filtered_cognitive_flist = glob.glob(join(beh_inputdir, sub,  '**','task-social', '**', f'*{bids_string}*.csv'), recursive=True)
     filtered_vicarious_flist = glob.glob(str(Path(beh_inputdir) / sub / '**' / 'task-social' / '**' / f'*{args.bids_string}*.csv'), recursive=True)
 
     if not filtered_vicarious_flist:
-        temp_fpath = Path(beh_inputdir) / sub / 'task-social' / ses / f'{sub}_{ses}_task-social_{run}*TEMP*.csv'
-        if temp_fpath.is_file():
-            filtered_vicarious_flist = [str(temp_fpath)]
+        
+        temp_fpath = glob.glob(Path(beh_inputdir) / sub / 'task-social' / ses / f'{sub}_{ses}_task-social_{run}*TEMP*.csv')
+        if temp_fpath[0].is_file():
+            filtered_vicarious_flist = [str(temp_fpath[0])]
         else:
             print(f'No behavior data file found for {sub}, {ses}, {run}. Checked both standard and temporary filenames.')
             filtered_vicarious_flist = []
