ab959d28537dbb9c3843767656050b367d8fd27f
jungheejung <heejung.jung@colorado.edu>
2024-09-01 14:56:59 -0400

    BUG: explicitly specify filepath

--- spacetop_prep/datalad/rename_sub-0075_ses-01_task-social.sh
+++ spacetop_prep/datalad/rename_sub-0075_ses-01_task-social.sh
@@ -5,22 +5,27 @@ set -eu
 RENAME_FUNC="/Users/h/Documents/projects_local/1076_spacetop/code/rename_file"
 
 # Define the base file name pattern
+SUBPATH="/Users/h/Documents/projects_local/1076_spacetop/sub-0075/ses-01/func"
 PATTERN="sub-0075_ses-01_task-social_acq-mb8_run"
 
 # Rename files
-$RENAME_FUNC "${PATTERN}-01_bold.json" "${PATTERN}-02_bold.json"
-$RENAME_FUNC "${PATTERN}-01_bold.nii.gz" "${PATTERN}-02_bold.nii.gz"
-$RENAME_FUNC "${PATTERN}-01_sbref.json" "${PATTERN}-02_sbref.json"
-$RENAME_FUNC "${PATTERN}-01_sbref.nii.gz" "${PATTERN}-02_sbref.nii.gz"
+$RENAME_FUNC "${SUBPATH}/${PATTERN}-01_bold.json" "${SUBPATH}/${PATTERN}-02_bold.json"
+$RENAME_FUNC "${SUBPATH}/${PATTERN}-01_bold.nii.gz" "${SUBPATH}/${PATTERN}-02_bold.nii.gz"
+$RENAME_FUNC "${SUBPATH}/${PATTERN}-01_sbref.json" "${SUBPATH}/${PATTERN}-02_sbref.json"
+$RENAME_FUNC "${SUBPATH}/${PATTERN}-01_sbref.nii.gz" "${SUBPATH}/${PATTERN}-02_sbref.nii.gz"
 
-$RENAME_FUNC "${PATTERN}-02_bold.json" "${PATTERN}-03_bold.json"
-$RENAME_FUNC "${PATTERN}-02_bold.nii.gz" "${PATTERN}-03_bold.nii.gz"
-$RENAME_FUNC "${PATTERN}-02_sbref.json" "${PATTERN}-03_sbref.json"
-$RENAME_FUNC "${PATTERN}-02_sbref.nii.gz" "${PATTERN}-03_sbref.nii.gz"
+$RENAME_FUNC "${SUBPATH}/${PATTERN}-02_bold.json" "${SUBPATH}/${PATTERN}-03_bold.json"
+$RENAME_FUNC "${SUBPATH}/${PATTERN}-02_bold.nii.gz" "${SUBPATH}/${PATTERN}-03_bold.nii.gz"
+$RENAME_FUNC "${SUBPATH}/${PATTERN}-02_sbref.json" "${SUBPATH}/${PATTERN}-03_sbref.json"
+$RENAME_FUNC "${SUBPATH}/${PATTERN}-02_sbref.nii.gz" "${SUBPATH}/${PATTERN}-03_sbref.nii.gz"
 
-$RENAME_FUNC "${PATTERN}-03_bold.json" "${PATTERN}-04_bold.json"
-$RENAME_FUNC "${PATTERN}-03_bold.nii.gz" "${PATTERN}-04_bold.nii.gz"
-$RENAME_FUNC "${PATTERN}-03_sbref.json" "${PATTERN}-04_sbref.json"
-$RENAME_FUNC "${PATTERN}-03_sbref.nii.gz" "${PATTERN}-04_sbref.nii.gz"
+$RENAME_FUNC "${SUBPATH}/${PATTERN}-03_bold.json" "${SUBPATH}/${PATTERN}-04_bold.json"
+$RENAME_FUNC "${SUBPATH}/${PATTERN}-03_bold.nii.gz" "${SUBPATH}/${PATTERN}-04_bold.nii.gz"
+$RENAME_FUNC "${SUBPATH}/${PATTERN}-03_sbref.json" "${SUBPATH}/${PATTERN}-04_sbref.json"
+$RENAME_FUNC "${SUBPATH}/${PATTERN}-03_sbref.nii.gz" "${SUBPATH}/${PATTERN}-04_sbref.nii.gz"
 
+$RENAME_FUNC "${SUBPATH}/${PATTERN}-04_bold.json" "${SUBPATH}/${PATTERN}-06_bold.json"
+$RENAME_FUNC "${SUBPATH}/${PATTERN}-04_bold.nii.gz" "${SUBPATH}/${PATTERN}-06_bold.nii.gz"
+$RENAME_FUNC "${SUBPATH}/${PATTERN}-04_sbref.json" "${SUBPATH}/${PATTERN}-06_sbref.json"
+$RENAME_FUNC "${SUBPATH}/${PATTERN}-04_sbref.nii.gz" "${SUBPATH}/${PATTERN}-06_sbref.nii.gz"
 # The run-06 files do not need renaming
