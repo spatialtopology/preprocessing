ebc986e0c0f0f196b1253dc87189bd6e70715780
jungheejung <heejung.jung@colorado.edu>
2024-09-01 16:10:24 -0400

    BUG: file name path error

--- spacetop_prep/datalad/resolve_missingevents/s04_sub_0001_task_tomsaxe.m
+++ spacetop_prep/datalad/resolve_missingevents/s04_sub_0001_task_tomsaxe.m
@@ -1,6 +1,6 @@
 % download https://github.com/spatialtopology/task-fractional/blob/DBIC/task-tomsaxe/answer_key.csv
 url = 'https://raw.githubusercontent.com/spatialtopology/task-fractional/DBIC/task-tomsaxe/answer_key.csv?token=GHSAT0AAAAAACV2UHC5PPWMDHRIW6RKIJWAZWUZB2A';
-answer_filename = 'answer_key.csv';
+answer_filename = '/Users/h/Documents/projects_local/1076_spacetop/code/spacetop-prep/spacetop_prep/datalad/answer_key.csv';
 outfilename = websave(answer_filename, url);
 
 events_fname = '/Users/h/Documents/projects_local/1076_spacetop/sourcedata/d_beh/sub-0001/task-fractional/sub-0001_ses-04_task-fractional_run-01-tomsaxe_beh.csv';
