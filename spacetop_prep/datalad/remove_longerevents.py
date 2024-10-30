3ba09c9ff26896f3464fd413c6247621f5a44eac
jungheejung <heejung.jung@colorado.edu>
2024-10-30 09:54:00 -0700

    DEV: remove rows from events.tsv based on BOLD image length; BUG: fix filename typo

--- spacetop_prep/datalad/remove_longerevents.py
+++ spacetop_prep/datalad/remove_longerevents.py
@@ -16,19 +16,19 @@ import json
 import pandas as pd
 # file list is identified via Yarik Halchenco. 
 # check out description in issue #110 in spatialtopology spacetop-prep
-flist = ['/sub-0013/ses-04/func/sub-0013_ses-04_task-fractional_acq-mb8_run-01_bold.nii.gz',
-         '/sub-0017/ses-02/func/sub-0017_ses-02_task-alignvideo_acq-mb8_run-01_bold.nii.gz',
-         '/sub-0017/ses-04/func/sub-0017_ses-04_task-social_acq-mb8_run-04_bold.nii.gz',
-         '/sub-0035/ses-01/func/sub-0035_ses-01_task-alignvideo_acq-mb8_run-02_bold.nii.gz',
-         '/sub-0055/ses-02/func/sub-0055_ses-02_task-narratives_acq-mb8_run-04_bold.nii.gz',
-         '/sub-0061/ses-01/func/sub-0061_ses-01_task-alignvideo_acq-mb8_run-03_bold.nii.gz',
-         '/sub-0069/ses-02/func/sub-0069_ses-02_task-narratives_acq-mb8_run-03_bold.nii.gz',
-         '/sub-0084/ses-03/func/sub-0084_ses-03_task-alignvideo_acq-mb8_run-01_bold.nii.gz']
+flist = ['sub-0013/ses-04/func/sub-0013_ses-04_task-fractional_acq-mb8_run-01_bold.nii.gz',
+         'sub-0017/ses-02/func/sub-0017_ses-02_task-alignvideo_acq-mb8_run-01_bold.nii.gz',
+         'sub-0017/ses-04/func/sub-0017_ses-04_task-social_acq-mb8_run-04_bold.nii.gz',
+         'sub-0035/ses-01/func/sub-0035_ses-01_task-alignvideo_acq-mb8_run-02_bold.nii.gz',
+         'sub-0055/ses-02/func/sub-0055_ses-02_task-narratives_acq-mb8_run-04_bold.nii.gz',
+         'sub-0061/ses-01/func/sub-0061_ses-01_task-alignvideo_acq-mb8_run-03_bold.nii.gz',
+         'sub-0069/ses-02/func/sub-0069_ses-02_task-narratives_acq-mb8_run-03_bold.nii.gz',
+         'sub-0084/ses-03/func/sub-0084_ses-03_task-alignvideo_acq-mb8_run-01_bold.nii.gz']
 # get BOLD data TR (dcmmeta_shape[-1])
 for fpath in flist:
     # Get the base filename and construct paths for the .json and events.tsv files
     basename = os.path.basename(fpath)
-    json_path = fpath.replace('_bold.nii.gz', '.json')
+    json_path = fpath.replace('_bold.nii.gz', '_bold.json')
     events_path = fpath.replace('_bold.nii.gz', '_events.tsv')
 
     # Load JSON metadata to retrieve BOLD TR duration
@@ -46,4 +46,4 @@ for fpath in flist:
     rows_removed = len(events_df) - len(filtered_events_df)
     print(f"The events.tsv for {basename} is {(events_df['onset'].iloc[-1])} long.\n The expected length should be {max_scan_time}. We'll shave off {rows_removed} rows to match expected length")
     # save file
-    #filtered_events_df.to_csv(events_path, sep='\t', index=False)
+    filtered_events_df.to_csv(events_path, sep='\t', index=False)
