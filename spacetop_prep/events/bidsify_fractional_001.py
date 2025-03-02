b8a24ef2d6ce5d0fc4557fff8c188fc6b4a7c20d
jungheejung <heejung.jung@colorado.edu>
2025-03-02 12:29:47 -0800

    BUG: add data filepath

--- spacetop_prep/events/bidsify_fractional_001.py
+++ spacetop_prep/events/bidsify_fractional_001.py
@@ -1,10 +1,12 @@
 import pandas as pd
+from os.path import join
 # load csv file
+file_dir = '/Users/h/Documents/projects_local/1076_spacetop'
 flist = [
     'sub-0001/ses-04/func/sub-0001_ses-04_task-fractional_acq-mb8_run-01_events.tsv', 
     'sub-0001/ses-04/func/sub-0001_ses-04_task-fractional_acq-mb8_run-02_events.tsv'
     ]
 for fname in flist:
-    df = pd.read_csv(fname, sep='\t')
+    df = pd.read_csv(join(file_dir, fname), sep='\t')
     newdf = df.sort_values(by=['onset'])
-    newdf.to_csv(fname, sep='\t')
+    newdf.to_csv(join(file_dir, fname), sep='\t')
