bf5a388f0721d56de3b57e925fa2da499b91c047
jungheejung <heejung.jung@colorado.edu>
2025-03-02 12:42:41 -0800

    BUG: remove index and sort onset

--- spacetop_prep/events/bidsify_fractional_001.py
+++ spacetop_prep/events/bidsify_fractional_001.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from os.path import join
+from pathlib import Path
 # load csv file
 file_dir = '/Users/h/Documents/projects_local/1076_spacetop'
 flist = [
@@ -7,6 +8,8 @@ flist = [
     'sub-0001/ses-04/func/sub-0001_ses-04_task-fractional_acq-mb8_run-02_events.tsv'
     ]
 for fname in flist:
-    df = pd.read_csv(join(file_dir, fname), sep='\t')
-    newdf = df.sort_values(by=['onset'])
-    newdf.to_csv(join(file_dir, fname), sep='\t')
+    if Path(join(file_dir, fname)).exists():
+        df = pd.read_csv(join(file_dir, fname), sep='\t')
+        newdf = df.sort_values(by=['onset'])
+        newdf_na = newdf.fillna('n/a')
+        newdf_na.to_csv(join(file_dir, fname), sep='\t', index=False)
